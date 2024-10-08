cddff328bfc95c102bce0effe875e65fc54a45ab
Valli V Narayanan <vallivn18@gmail.com>
2024-10-09 00:17:30 +0530

    Added Vercel Config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const MONGO_URI= "mongodb+srv://admin:admin@cluster0.rwx7w.mongodb.net/inventory?retryWrites=true&w=majority&appName=Cluster0"
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 15000, // Increase timeout to 15 seconds
--- server.js
+++ server.js
@@ -24,3 +24,5 @@ app.use('/', inventoryRoutes);
 // Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+module.exports = app;
